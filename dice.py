711c04810c452b21ee113cb5b5849ede495fa261
Zai-Lucas <56050470+Zai-Lucas@users.noreply.github.com>
2019-10-02 15:04:14 -0700

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,14 @@
+# Name: Isaiah Lucas
+# 6 Period 
+# Dice Rolling Simulator
+import random 
+random.randint(1,6)
+
+rolls = int(input("how many rolls "))
+x = 1
+while x <= rolls:
+	x = x + 1
+if x >= 100:
+	print
+
+
