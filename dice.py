f8af4a2e9b5a47596175108768678fc47ad72e1a
Zai-Lucas <56050470+Zai-Lucas@users.noreply.github.com>
2019-10-03 15:02:38 -0700

    Generates #s 1 - 6

--- dice.py
+++ dice.py
@@ -2,13 +2,29 @@
 # 6 Period 
 # Dice Rolling Simulator
 import random 
-random.randint(1,6)
+random.randint(1, 6)
+
+numOne = 1
+numTwo = 0
+numThree = 0
+numFour = 0
+numFive = 0
+numSix = 0
+
 
 rolls = int(input("how many rolls "))
 x = 1
 while x <= rolls:
 	x = x + 1
-if x >= 100:
-	print
+	x <= 100
+	print(random.randint(1,6))
+
+def printNum():
+	print("1s" + str(NumOne))
+
+	
+
+
+
 
 
